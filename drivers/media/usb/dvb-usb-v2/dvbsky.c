winiq/linux_media

media: dvb-usb-v2\dvbsky: MyGica T230C v2 Lite support.

drivers/media/usb/dvb-usb-v2/dvbsky.c
@@ -541,7 +541,8 @@ static int dvbsky_mygica_t230c_attach(struct dvb_usb_adapter *adap)
 	si2168_config.i2c_adapter = &i2c_adapter;
 	si2168_config.fe = &adap->fe[0];
 	si2168_config.ts_mode = SI2168_TS_PARALLEL;
-	if (le16_to_cpu(d->udev->descriptor.idProduct) == USB_PID_MYGICA_T230C2)
+	if (le16_to_cpu(d->udev->descriptor.idProduct) == USB_PID_MYGICA_T230C2 ||
+	    le16_to_cpu(d->udev->descriptor.idProduct) == USB_PID_MYGICA_T230C2_LITE)
 		si2168_config.ts_mode |= SI2168_TS_CLK_MANUAL;
 	si2168_config.ts_clock_inv = 1;
 
@@ -789,6 +790,9 @@ static const struct usb_device_id dvbsky_id_table[] = {
 	{ DVB_USB_DEVICE(USB_VID_CONEXANT, USB_PID_MYGICA_T230C2,
 		&mygica_t230c_props, "MyGica Mini DVB-(T/T2/C) USB Stick T230C v2",
 		RC_MAP_TOTAL_MEDIA_IN_HAND_02) },
+	{ DVB_USB_DEVICE(USB_VID_CONEXANT, USB_PID_MYGICA_T230C2_LITE,
+		&mygica_t230c_props, "MyGica Mini DVB-(T/T2/C) USB Stick T230C v2  Lite",
+		NULL) },
 	{ }
 };
 MODULE_DEVICE_TABLE(usb, dvbsky_id_table);

mich2200/media_tree_cc

drivers/media/usb/dvb-usb-v2/dvbsky.c
@@ -544,7 +544,8 @@ static int dvbsky_mygica_t230_attach(struct dvb_usb_adapter *adap)
 	si2168_config.i2c_adapter = &i2c_adapter;
 	si2168_config.fe = &adap->fe[0];
 	si2168_config.ts_mode = SI2168_TS_PARALLEL;
-	if (le16_to_cpu(d->udev->descriptor.idProduct) == USB_PID_MYGICA_T230C2)
+	if (le16_to_cpu(d->udev->descriptor.idProduct) == USB_PID_MYGICA_T230C2 ||
+	    le16_to_cpu(d->udev->descriptor.idProduct) == USB_PID_MYGICA_T230C2_LITE)
 		si2168_config.ts_mode |= SI2168_TS_CLK_MANUAL;
 	si2168_config.ts_clock_inv = 1;
 
@@ -558,7 +559,8 @@ static int dvbsky_mygica_t230_attach(struct dvb_usb_adapter *adap)
 	si2157_config.fe = adap->fe[0];
 	switch (le16_to_cpu(d->udev->descriptor.idProduct)) {
 	case USB_PID_MYGICA_T230C:
-	case USB_PID_MYGICA_T230C2: {
+	case USB_PID_MYGICA_T230C2:
+	case USB_PID_MYGICA_T230C2_LITE: {
 		si2157_config.if_port = 0;
 		state->i2c_client_tuner = dvb_module_probe("si2157", "si2141",
 							   i2c_adapter,
@@ -909,6 +911,9 @@ static const struct usb_device_id dvbsky_id_table[] = {
 	{ DVB_USB_DEVICE(USB_VID_CONEXANT, USB_PID_MYGICA_T230C2,
 		&mygica_t230_props, "MyGica Mini DVB-(T/T2/C) USB Stick T230C v2",
 		RC_MAP_TOTAL_MEDIA_IN_HAND_02) },
+	{ DVB_USB_DEVICE(USB_VID_CONEXANT, USB_PID_MYGICA_T230C2_LITE,
+		&mygica_t230_props, "MyGica Mini DVB-(T/T2/C) USB Stick T230C v2  Lite",
+		NULL) },
 	{ DVB_USB_DEVICE(USB_VID_GTEK, 0xD220,
 		&mygica_t220_props, "MyGica DVB-(T/T2) USB Stick T220",
 		RC_MAP_TOTAL_MEDIA_IN_HAND_02) },